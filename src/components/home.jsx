1327c2c4a627955ded584223976838e70587001f
relish <guptarelish333@gmail.com>
2023-04-19 20:21:09 +0530

    adding links for demo and code button

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
-        <div className='flex flex-col justify-center mt-[200px] md:mt-5'>
+        <div className='flex flex-col justify-center mt-[150px] md:mt-5'>
           <h2 className='text-3xl sm:text-7xl font-bold text-white'>I'm a Frontend Developer</h2>
           <p className='text-gray-500 py-2 max-w-xl'>
           As a frontend developer, I specialize in building responsive and user-friendly websites using the latest technologies such as HTML, CSS, and JavaScript. I am proficient in various frameworks such as React and Tailwind and have experience with version control systems such as Git. Looking for an internship opportunity to further develop my skills and contribute to exciting projects in the field of web development.
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -10,29 +10,55 @@ const Projects = () => {
     {
       id:1,
       src:portfolio,
+      title:'Portfolio',
+      url:'https://relishgupta-portfolio.netlify.app/',
+      code:'https://github.com/relishgupta/Portfolio',
     },
     {
       id:2,
       src:crownclothing,
+      title:'Ecommerce website',
+      url:'https://crown-clothing-relishgupta.netlify.app/',
+      code:'https://github.com/relishgupta/Ecommerce-website',
     },
     {
       id:3,
       src:crowntunes,
+      title:'Music website',
+      url:'https://crowntunes-relishgupta.netlify.app/',
+      code:'https://github.com/relishgupta/music-website',
     },
     {
       id:4,
       src:quote,
+      title:'Random Quote Generator',
+      url:'https://quote-generator-1b09d2.netlify.app/',
+      code:'https://github.com/relishgupta/advice-app',
     },
     {
       id:5,
       src:spotify,
+      title:'Leave Management System',
+      url:'https://relishgupta.github.io/Spotify/',
+      code:'https://github.com/relishgupta/Spotify',
     },
     {
       id:6,
       src:game,
       style:"mob:hidden",
+      title:'Action game',
+      url:'https://cozy-panda-215139.netlify.app/',
+      code:'https://github.com/relishgupta/ActionGame',
     },
   ];
+
+  const Deploy=(({url})=>{
+      window.open(url,'_blank');
+  })
+
+  const Repo=(({code})=>{
+    window.open(code,'_blank');
+})
 
   return (
     <div name="projects" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
@@ -46,19 +72,19 @@ const Projects = () => {
           </p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {projects.map(({src,id,style})=>(
+          {projects.map(({src,id,style,title,url,code})=>(
             <div key={id} className={`shadow-md shadow-gray-600 rounded-lg + ${style}`}>
-                 <span><p className="text-base">Portfolio</p></span>
+                 <span><p className="text-base">{title}</p></span>
                 <img
                   src={src}
                   alt=""
                   className="rounded-md duration-200 hover:scale-105 "
                 />
                 <div className="flex items-center justify-center">
-                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <button onClick={()=>{Deploy({url})}} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                     Demo
                   </button>
-                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <button onClick={()=>{Repo({code})}} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                     Code
                   </button>
                 </div>
