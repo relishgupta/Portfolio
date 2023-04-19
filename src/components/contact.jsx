ebd085559cf14c7d16ea7f2ccbb3c3a46e18acf0
relish <guptarelish333@gmail.com>
2023-04-19 19:10:13 +0530

    adding links for demo and code button

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -82,7 +82,7 @@ const Contact = () => {
           </form>
         </div>
         <div className="flex flex-col top-[35%] md:hidden">
-          <ul className="mob:mt-[-63px] mob:ml-[50px]">
+          <ul className="mob:mt-[-61px] mob:ml-[50px]">
             {links.map(({id,child,href,style,download})=>(
               <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[100px] mt-2 rounded-md " + style}>
                 <a href={href} className="flex justify-between items-center w-full text-white" target="_blank" rel="noreferrer">
