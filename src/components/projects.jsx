6b59227a4ee5f7b83d7f0cf48c619757e7fd11cc
relish <guptarelish333@gmail.com>
2023-04-19 20:44:03 +0530

    updated projects section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -63,7 +63,7 @@ const Projects = () => {
   return (
     <div name="projects" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="mob:pb-8 sm:pb-1">
+        <div className="mob:pb-8 sm:pb-0">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Projects
           </p>
